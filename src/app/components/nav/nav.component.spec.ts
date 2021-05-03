e3c9b2cb53b50096ab81f0bb7983e192a9472ecd
Steven Pazur <sjpazur@yahoo.com>
2021-05-03 07:56:37 -0700

    created route for landingpage localhost:4200/

--- src/app/components/nav/nav.component.spec.ts
+++ src/app/components/nav/nav.component.spec.ts
@@ -1,16 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      imports: [ AppRoutingModule ]
     })
     .compileComponents();
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
   });
 
   beforeEach(() => {
@@ -22,4 +30,10 @@ describe('NavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be able to route to LandingPageComponent by navigating to http://localhost:4200/', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('');
+  }));
 });
