63e3fdab5305685d9649b713dc0ac580e3821070
raaedk <raaedk@gmail.com>
2021-05-04 14:28:12 -0400

    updated route

--- src/app/modules/character-routing.module.ts
+++ src/app/modules/character-routing.module.ts
@@ -10,7 +10,7 @@ export const routes : Routes = [
     component: CharacterListComponent
   },
 
-  { path: 'character/:id', component : CharacterDetailsComponent},
+  { path: ':id', component : CharacterDetailsComponent},
 ]
 
 @NgModule({
